d7578018bf6f2b5082567982425eea65e9095fab
econibear <emilyconibear@hotmail.co.uk>
2024-12-20 16:46:50 +0000

    tried to fix it but broken

--- Module2_Assignment1_AirPollution.R
+++ Module2_Assignment1_AirPollution.R
@@ -10,19 +10,28 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
   file_list = list.files(directory, pattern=".csv", full.names = TRUE)
 
   df = data.frame()
+  total_df = data.frame()
   subset_data = file_list[id]
 
   for (i in id) {
     df = read.csv(subset_data[i])
+    
+    total_df = rbind(total_df, df)
   }
-  
-  pollutant_data <- df[[pollutant]]
+  pollutant_data <- total_df[[pollutant]]
   x <- mean(pollutant_data, na.rm=TRUE)
   x
 
 }
 
-pollutantmean(directory1, "sulfate", 1:10)
+pollutantmean(directory1, "sulfate", 1:10) #works
 
+
+pollutantmean(directory1, "nitrate", 70:72) #broken
+
+
+pollutantmean(directory1, "nitrate", 23) #broken
+
+df = read.csv("~/Documents/Coursera_DataScienceSpecialisation/Assignments/2_RProgramming_ProgrammingAssignment1/specdata/023.csv")
 
 
